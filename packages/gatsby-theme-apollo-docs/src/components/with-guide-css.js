fc8000e0f2d4adf90fb472918f7964244b198309
Loren 🤓 <lorensr@gmail.com>
2021-05-28 23:32:30 -0400

    Update meta description

--- packages/gatsby-theme-apollo-docs/src/components/with-guide-css.js
+++ packages/gatsby-theme-apollo-docs/src/components/with-guide-css.js
@@ -10,7 +10,7 @@ export default ({children}) => (
         siteName="The GraphQL Guide"
         favicon="/favicon.ico"
         twitterHandle="graphqlguide"
-        description="A comprehensive book from John Resig, the creator of jQuery. The complete guide to GraphQL, from a beginner introduction to advanced client and server topics."
+        description="The complete guide to GraphQL, from a beginner introduction to advanced client and server topics. A comprehensive 886-page book from John Resig, the creator of jQuery."
         image="https://res.cloudinary.com/graphql/header"
       />
     {children}
